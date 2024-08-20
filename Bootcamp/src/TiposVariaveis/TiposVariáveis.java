09a783fb901a7f6ad1435ef8f712b933dc116b51
Leoman <leoman.cassio@hotmail.com>
2024-08-20 17:34:24 -0300

    .

--- Bootcamp/src/TiposVariaveis/TiposVariáveis.java
+++ Bootcamp/src/TiposVariaveis/TiposVariáveis.java
@@ -1,9 +1,25 @@
 package TiposVariaveis;
 
 public class TiposVariáveis {
-    byte idade;  //-128 a 127
-    short codigo; // -32k a 32k
-    int muitos; //-2Bi a 2Bi
-    long slk; // muitos a muitos
+    public static void main(String[] args) {
+        byte idade;  //-128 a 127
+        short ano; // -32k a 32k
+        int muitos; //-2Bi a 2Bi
+        long slk; // muitos a muitos
+        final int RODAS =  4; //Constante
+
+        String nome = "Leoman";
+        String nomeCompleto = nome.concat(" ").concat("Cássio"); //Concatenação
+        System.out.println(nomeCompleto);
+
+        muitos =  1000;
+        ano = (short) muitos;
+        System.out.println(ano);
+        slk = 12;
+        muitos = (int) slk;
+        System.out.println(muitos);
+
+    }
+
 
 }
