ff8ffaf074d23f3b348b4af17432bbab47a94b29
Leoman <leoman.cassio@hotmail.com>
2024-08-20 17:04:25 -0300

    .

--- Bootcamp/src/TiposVariaveis/TiposVariáveis.java
+++ Bootcamp/src/TiposVariaveis/TiposVariáveis.java
@@ -1,5 +1,9 @@
 package TiposVariaveis;
 
 public class TiposVariáveis {
+    byte idade;  //-128 a 127
+    short codigo; // -32k a 32k
+    int muitos; //-2Bi a 2Bi
+    long slk; // muitos a muitos
 
 }
